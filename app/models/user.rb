e920258806ad02029393f77c7dfd11f71ad4c569
Oscar Tellez <oscartzgz@gmail.com>
2018-11-29 23:07:57 -0600

    Validate name presence true con user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :name, presence: true
 end
